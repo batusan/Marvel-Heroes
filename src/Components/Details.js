da6028a96e38b6470f710f937cb17cd4e2317171
Batuhan San <batuhansann@gmail.com>
2022-04-24 16:21:41 +0300

    Back button added

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { getCharacterDetail } from "../Services/characterService";
 import Loader from "./Loader";
+import leftArrow from "../assets/left_arrow.png";
+import { useNavigate } from "react-router-dom";
 
 function Details({ heroId }) {
   const [loading, setLoading] = useState(true);
   const [character, setCharacter] = useState();
   const [stories, setStories] = useState();
   const [series, setSeries] = useState();
+
+  const navigator = useNavigate();
 
   const { t } = useTranslation();
 
@@ -39,6 +43,14 @@ function Details({ heroId }) {
           </div>
           <div className="hero-detail hero-font">
             <div id="hero-detail-name">
+              <img
+                src={leftArrow}
+                alt="back button"
+                className="hero-detail-button "
+                onClick={() => {
+                  navigator("/");
+                }}
+              />{" "}
               {character.name} <hr />
             </div>
             <div id="hero-detail-description">{character.description}</div>
